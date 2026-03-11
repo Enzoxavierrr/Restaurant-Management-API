06b7654b81599d1f52f183746fc1f17fe5744b1f
Enzo <e.pecantet@edu.pucrs.br>
2026-03-11 20:37:10 -0300

    feat: Adiciona estrutura inicial do projeto com Maven e Spring Boot, incluindo arquivos de configuração, classes principais e dependências necessárias.

---
+++ src/test/java/com/enzo/restaurant_api/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzo.restaurant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
