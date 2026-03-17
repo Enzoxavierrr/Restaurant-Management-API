7841f42184e7a3750502ca5882f3d8b52089f2f2
Enzo <e.pecantet@edu.pucrs.br>
2026-03-17 19:30:45 -0300

    feat: adiciona classes DTO para mapeamento de dados

---
+++ src/main/java/com/enzo/restaurant_api/dto/CreateUserRequest.java
@@ -0,0 +1,16 @@
+package com.enzo.restaurant_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/enzo/restaurant_api/dto/OrderItemRequest.java
@@ -0,0 +1,15 @@
+package com.enzo.restaurant_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemRequest {
+    private Long productId;
+    private Integer quantity;
+}
